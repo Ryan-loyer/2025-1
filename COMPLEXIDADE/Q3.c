e2a0b4be9f551a93fc6777ac749ec83ba5af306d
Pablo Ryan <pabloryanlopes@gmail.com>
2025-06-06 20:21:28 -0300

    Update

--- COMPLEXIDADE/Q3.c
+++ COMPLEXIDADE/Q3.c
@@ -2,7 +2,7 @@
 Aréa de teste do código:
 
 (gdb) start
-Temporary breakpoint 1 at 0x4014a8: file c:\Users\1mayc\OneDrive\Desktop\AEII\projeto_1u\Estruturas-de-dados-II\EDII\Listas\exercicios complexidade\Q2.c, line 15.
+Temporary breakpoint 1 at 0x4014a8: file c:\Users\pablo\OneDrive\Desktop\COMPLEXIDADE\projeto\exercicios complexidade\Q2.c, line 15.
 Starting program: C:\Users\1mayc\OneDrive\Desktop\AEII\projeto_1u\Estruturas-de-dados-II\EDII\Listas\exercicios complexidade\output/Q2.exe
 [New Thread 17228.0xa64]
 [New Thread 17228.0x3ed8]
